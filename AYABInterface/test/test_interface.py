fbfe05b5e984edfe89ae724dcea952c8fb5cdff7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-22 23:39:22 +0200

    added utisl test

--- AYABInterface/test/test_interface.py
+++ AYABInterface/test/test_interface.py
@@ -27,35 +27,3 @@ def communication():
 @fixture
 def interface(configuration, communication):
     return Interface(configuration, communication)
-
-
-def test_get_communication(interface, communication):
-    assert interface.communication == communication
-
-
-def test_get_initial_confguration(interface, configuration):
-    assert interface.initial_configuration == configuration
-
-
-def test_machine(interface, configuration):
-    assert interface.machine == configuration.machine
-
-
-def test_current_row_is_not_knit(interface, configuration):
-    row = interface.current_row
-    assert len(row) == configuration.number_of_needles
-    assert row == [-1] * configuration.number_of_needles
-
-
-def test_configured_rows(interface, communication, rows):
-    """Rows with two colors can be knit easily."""
-    communication.initialize.assert_called_once(rows, interface.observer)
-
-
-def test_index_in_rows(interface):
-    assert interface.index_of_current_row == 1
-
-
-def test_knit_one_row(interface):
-    interface.observer.row_completed(1)
-    assert interface.
---
+++ AYABInterface/test/test_utils.py
@@ -0,0 +1,29 @@
+"""Test utility methods."""
+
+from AYABInterface.interface import sum_all, number_of_colors
+
+
+class TestSumAll(object):
+
+    """Test :func:`AYABInterface.interface.sum_all`."""
+
+    def test_integers(self):
+        """Sum up :class:`integers <int>`."""
+        assert sum_all([1, 2, 3, 4], 2) == 12
+
+    def test_lists(self):
+        """Sum up :class:`lists <list>`."""
+        assert sum_all([[1], [2, 3], [4]], [9]) == [9, 1, 2, 3, 4]
+
+    def test_sets(self):
+        """Sum up :class:`sets <set>`."""
+        assert sum_all(map(set, [[1, 2], [3, 5], [3, 1]], set([0, 5]))) == \
+            set([0, 1, 2, 3, 5])
+
+    
+@pytest.mark.parametrize("colors,number",
+    [[[[1, 2, 3], [2, 3, 3, 3], [2, 2, 2], [0]], 4],
+    [[[], [], []], 0], [[[1, 1, 1], ["", "q"], ["asd"]], 4]])
+def test_number_of_colors(colors, number):
+    """Test :func:`AYABInterface.interface.number_of_colors`."""
+    assert number_of_colors(colors) == number
