10356207d293ca059a344dc4a345f5f60acee150
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-24 00:03:15 +0200

    made coloring tests apsss

--- AYABInterface/convert/__init__.py
+++ AYABInterface/convert/__init__.py
@@ -1,9 +1,14 @@
 """Conversion of colors to needle positions."""
 from collections import namedtuple
 
-NeedlePositions = namedtuple("NeedlePositions", [])
+NeedlePositions = namedtuple("NeedlePositions", ["needle_coloring", "colors",
+                                                 "two_colors"])
 
 
+def _row_color(row, color):
+    return [(0 if color_ == color else 1) for color_ in row]
+                                                 
+                                                 
 def colors_to_needle_positions(rows):
     """Convert rows to needle positions.
     
@@ -11,5 +16,26 @@ def colors_to_needle_positions(rows):
     :rtype: list
     """
     needles = []
-    
+    for row in rows:
+        colors = set(row)
+        if len(colors) == 1:
+            needles.append([NeedlePositions(row, tuple(colors), False)])
+        elif len(colors) == 2:
+            color1, color2 = colors
+            if color1 != row[0]:
+                color1, color2 = color2, color1
+            needles_ = _row_color(row, color1)
+            needles.append([NeedlePositions(needles_, (color1, color2), True)])
+        else:
+            colors = []
+            for color in row:
+                if color not in colors:
+                    colors.append(color)
+            needles_ = []
+            for color in colors:
+                needles_.append(NeedlePositions(_row_color(row, color), 
+                                                (color,), False))
+            needles.append(needles_)
+    return needles
+
 __all__ = ["colors_to_needle_positions"]
--- AYABInterface/convert/test/test_colors_to_needle_positions.py
+++ AYABInterface/convert/test/test_colors_to_needle_positions.py
@@ -7,11 +7,11 @@ class TestColorsToBinary(object):
     """Test :func:`AYABInterface.convert.colors_to_needle_positions`."""
 
     @pytest.mark.parametrize("rows,expected_needles", [
-        [[[0, 0, 0, 0]], [([0, 0, 0, 0], (0,), False)]],  # single color
-        [[[1, 2, 1, 2]], [([0, 1, 0, 1], (1, 2), True)]],  # two colors
-        [[[1, 2, 0, 2]], [([0, 1, 1, 1], (1,), False),  # three colors
-                          ([1, 0, 1, 0], (2,), False),
-                          ([1, 1, 0, 1], (0,), False)]],
+        [[[0, 0, 0, 0]], [[([0, 0, 0, 0], (0,), False)]]],  # single color
+        [[[1, 2, 1, 2]], [[([0, 1, 0, 1], (1, 2), True)]]],  # two colors
+        [[[1, 2, 0, 2]], [[([0, 1, 1, 1], (1,), False),  # three colors
+                           ([1, 0, 1, 0], (2,), False),
+                           ([1, 1, 0, 1], (0,), False)]]],
         ])
     def test_conversion(self, rows, expected_needles):
         needles = colors_to_needle_positions(rows)
@@ -19,7 +19,7 @@ class TestColorsToBinary(object):
 
     def test_attributes(self):
         needles = colors_to_needle_positions([[0, 0]])
-        row = needles[0]
+        row = needles[0][0]
         assert row[0] == row.needle_coloring
         assert row[1] == row.colors
         assert row[2] == row.two_colors
