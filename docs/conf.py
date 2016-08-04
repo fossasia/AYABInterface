8aaaf56143c82573f7d6a626b4009f051e55c9c9
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-08-05 00:05:41 +0200

    added documentation links

--- docs/conf.py
+++ docs/conf.py
@@ -446,5 +446,7 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/3/': None,
-                       'http://pyserial.readthedocs.io/en/latest/': None}
+intersphinx_mapping = {
+    'https://docs.python.org/3/': None,
+    'http://knittingpattern.readthedocs.io/en/latest/': None,
+    'http://pyserial.readthedocs.io/en/latest/': None}
