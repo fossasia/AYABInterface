2b4053c379181d9c308a6b3a17bc7e561a57b142
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-08-05 21:40:09 +0200

    changed docs author

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'AYABInterface'
 copyright = '2016, AllYarnsAreBeautiful & FOSSASIA'
-author = 'Nicco Kunzmann'
+author = 'AllYarnsAreBeautiful & FOSSASIA'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
